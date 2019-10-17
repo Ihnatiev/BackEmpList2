e0e6948bc048c31fc4c19d6ca33d7620d8350de9
Valerii <Ihnatiev@users.noreply.github.com>
2019-10-18 00:28:48 +0300

    v2.0.2

--- build/backend/config/secret.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwtSecret: "this_secret_should_be_longer"
-};
-//# sourceMappingURL=secret.js.map
--- build/backend/interfaces/IEmployee.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class IEmployee {
-}
-exports.IEmployee = IEmployee;
-//# sourceMappingURL=IEmployee.js.map
